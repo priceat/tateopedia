ff10817aaad35817d7a5ad9016bf3a3f888e7804
Tate Price <priceat@gmail.com>
2014-12-04 14:39:30 -0600

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,11 @@ Tateopedia::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host 'tateopedia.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+   config.assets.initialize_on_precompile = false 
+
 end
