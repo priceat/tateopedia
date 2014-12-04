8eeca48f7347ebe9502840aba4966ee973b96147
Tate Price <priceat@gmail.com>
2014-12-04 14:33:25 -0600

    clean

--- db/schema.rb
+++ db/schema.rb
@@ -25,12 +25,12 @@ ActiveRecord::Schema.define(version: 20141201222416) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "email",                  default: "",         null: false
-    t.string   "encrypted_password",     default: "",         null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,          null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20141201222416) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "role",                   default: "standard"
+    t.string   "role"
     t.string   "stripe_customer_id"
     t.boolean  "member",                 default: false
   end
